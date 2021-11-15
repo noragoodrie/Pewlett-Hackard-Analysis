37e45871cc028519042117fa6696498fc97c9089
Nora Goodrie <noragoodrie@Noras-MacBook-Air.local>
2021-11-14 19:11:01 -0800

    Completed Challenge

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -6,45 +6,43 @@ Select e.emp_no,
 		t.title,
 		t.from_date,
 		t.to_date
---INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
-INNER JOIN titles as t
+LEFT JOIN titles as t
 on(e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no ASC;
+ORDER BY e.emp_no ASC;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
-rt.first_name,
-rt.last_name,
-rt.title
+SELECT DISTINCT ON (emp_no)emp_no,
+	first_name,
+	last_name,
+	title
 --INTO unique_titles
-FROM retirement_titles as rt
-ORDER BY emp_no, to_date DESC;
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
 
 --Number of employees by most recent job title who are about to retire
-SELECT COUNT(uq.emp_no),uq.title
+SELECT COUNT(title), title
 --INTO retiring_titles
-FROM unique_titles as uq
+FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
-
 --DELIVERABLE 2--
-SELECT*FROM dept_emp;
-SELECT DISTINCT ON(e.emp_no)e.emp_no,
-e.first_name,
-e.last_name,
-e.birth_date,
-de.from_date,
-de.to_date,
-t.title
---INTO mentor_eligiblity
-FROM employees as e
-INNER JOIN dept_emp as de
-ON(e.emp_no = de.emp_no)
-INNER JOIN titles as t
-on(e.emp_no = t.emp_no)
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	t.title
+--INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS d
+ON e.emp_no = d.emp_no
+INNER JOIN titles AS t
+ON e.emp_no = t.emp_no
 WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
-ORDER BY emp_no;
+AND (d.to_date = '9999-01-01')
+ORDER BY e.emp_no;
